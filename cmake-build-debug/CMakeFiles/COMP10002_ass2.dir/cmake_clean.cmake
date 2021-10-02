57fc52597f5f753e0d0811743f1472a5d2933405
Leo <leolin.360@gmail.com>
2021-10-02 23:40:54 +1000

    Clion project creation

---
+++ cmake-build-debug/CMakeFiles/COMP10002_ass2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COMP10002_ass2.dir/main.c.o"
+  "COMP10002_ass2"
+  "COMP10002_ass2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/COMP10002_ass2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
